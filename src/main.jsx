82d3998c3d1142b4fe16b8eb0c9efdbebea8f3e8
nani0917 <nani820917@gmail.com>
2025-02-19 15:13:06 +0800

    更新

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { RouterProvider } from 'react-router-dom';
 import router from './router'
 
